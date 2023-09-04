18f9023db3f6d5bfce5eddde10c218a6b250f4af
ellukasion <lukasion@gmail.com>
2023-09-04 15:25:17 +0200

    Create basic components

---
+++ components/Heading.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <ul class="menu__list">
+            <li>Strona główna</li>
+            <li>Kontakt</li>
+        </ul>
+    </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <Heading />
+
+    <Content />
+
+    <Footer />
+</template>
